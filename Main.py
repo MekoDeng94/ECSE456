adeae207e5f30588182f683f2a583a789b2f68aa
Meko Deng <mekodeng94@gmail.com>
2018-02-10 12:24:08 -0500

    skeleton

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
 import random
 import os
 import sys
+
+#Not really sure yet how to code this.. (I don't know what structure we should adopt. @Ilyas and @Thasan? Maybe set a skeleton?)
+
+#For Francois, what do you think about writing a script with python to automate filtering through the data? Essentially, 
+#we need to figure out what type of file we want our filter data to be stored into, maybe in a txt format? or maybe a 
+#database. In the later case, we'll need to use MySQL probably.. and we'll need to learn how to use it.use
+
+
