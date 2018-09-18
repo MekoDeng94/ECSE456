5db18d98348df693ba3ee4658b0e2297ac65fc89
Tharsan <tharsanrp@gmail.com>
2018-09-18 19:04:16 -0400

    Technical Commit

--- googleScapper.py
+++ googleScapper.py
@@ -2,9 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from langdetect import detect
+from textblob import TextBlob
+
 
 class Scrape:
-    def __init__(self, term, startDate, endDate):
+    def __init__(self, term, startDate, endDate, idealNumberOfArticles=10):
         """
         :param term: The term you want to search (string)
         :param startDate: The start date mm/dd/yyyy (string)
@@ -13,10 +15,13 @@ class Scrape:
         self.term = term
         self.startDate = startDate
         self.endDate = endDate
-        self.url = 'https://www.google.com/search?q={0}&tbs=cdr:1,cd_min:{1},cd_max:{2}&tbm=nws'.format(self.term, self.startDate, self.endDate)
+        self.idealNumberOfArticles = idealNumberOfArticles
+        self.url = 'https://www.google.com/search?q={0}&num={1}&tbs=cdr:1,cd_min:{2},cd_max:{3}&tbm=nws'.format(
+            self.term, self.idealNumberOfArticles, self.startDate, self.endDate)
 
     def run(self):
-        USER_AGENT = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+        USER_AGENT = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
         response = requests.get(self.url, headers=USER_AGENT)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -35,8 +40,12 @@ class Scrape:
                 title = title.get_text()
                 if description:
                     description = description.get_text()
+                    textBlob = TextBlob(description)
                 if link != '#' and detect(description) == 'en':
-                    found_results.append({'keyword': self.term, 'rank': rank, 'title': title, 'description': description, 'link': link, 'language':detect(description)})
+                    found_results.append(
+                        {'keyword': self.term, 'rank': rank, 'title': title, 'description': description, 'link': link,
+                         'language': detect(description), 'sentiment': textBlob.sentiment.polarity,
+                         'subjectivity': textBlob.subjectivity})
                     rank += 1
 
         for articles in found_results:
@@ -46,5 +55,6 @@ class Scrape:
         TAG_RE = re.compile(r'<[^>]+>')
         return TAG_RE.sub('', text)
 
-a = Scrape('amd', '09/01/2018', '09/01/2018')
+
+a = Scrape('Canada', '09/10/2018', '09/10/2018', 100)
 a.run()
